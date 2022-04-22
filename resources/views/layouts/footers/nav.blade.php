fdeac16e292dcec84d4472bcbf271891c5d8d071
Komarru Jaman <Natsufull04@gmail.com>
2022-04-22 19:55:36 +0700

    edit navbar

--- resources/views/layouts/footers/nav.blade.php
+++ resources/views/layouts/footers/nav.blade.php
@@ -1,11 +1,11 @@
 <div class="row align-items-center justify-content-xl-between">
-    <div class="col-xl-">
+    <div class="col-xl">
         <div class="copyright text-center text-xl-left text-muted">
             &copy; {{ now()->year }} <a href="https://www.creative-tim.com" class="font-weight-bold ml-1" target="_blank">Creative Tim</a> &amp;
             <a href="https://www.updivision.com" class="font-weight-bold ml-1" target="_blank">Updivision</a>
         </div>
     </div>
-    <div class="col-xl-3">
+    <!-- <div class="col-xl-3">
         <ul class="nav nav-footer justify-content-center justify-content-xl-end">
             <li class="nav-item">
                 <a href="https://www.creative-tim.com" class="nav-link" target="_blank">Creative Tim</a>
@@ -20,5 +20,5 @@
                 <a href="http://blog.creative-tim.com" class="nav-link" target="_blank">Blog</a>
             </li>
         </ul>
-    </div>
+    </div> -->
 </div>
